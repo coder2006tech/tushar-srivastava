83ba0910eee75dcc2fa3a298212f7cb4bd22ac55
coder2006tech <prashantsln464@gmail.com>
2024-12-18 16:35:20 +0530

    Add files via upload

---
+++ 03-conversionoperation.js
@@ -0,0 +1,88 @@
+ /*let score = 33  // number 
+ let tushar = "33"  // string 
+ let shishir = "33abc"  // number 
+ let aditya = null  // 0
+ let satya = undefined  // NaN
+ let shriya = true  // 1 
+ let gauri =false //0
+ let x = "Aparna" // NaN  */
+
+
+ /*console.log(typeof score );
+ console.log(typeof (score));
+let valueInNumber = Number(x)
+console.log(typeof valueInNumber);
+console.log(valueInNumber);   NaN
+console.log(valueInNumber);*/
+
+
+ /*"33" = 33
+ "33abc" = NaN
+true = 1 ; false = 0
+
+
+let isloggedIN = "tushar"
+
+let booleanIsloggedIn = Boolean(isloggedIN)
+console.log(booleanIsloggedIn);
+
+1=true ; 0= false 
+"" = false
+"tushar"= true
+
+
+let someNumber = 33
+
+let stringNumber = String(someNumber)
+console.log(stringNumber);
+console.log( typeof stringNumber);  */
+
+//****************** OPERATIONS******************** 
+
+/*let value = 3
+let negValue = -value
+console.log(negValue); */
+
+/*console.log(2+2);
+console.log(2+2);
+console.log(2+2);
+console.log(2+2);
+console.log(2+2);
+console.log(2/%2);  // remainder */
+
+/*let str1 ="hello"
+let str2 = " tushar"
+
+let str3 = str1 + str2*/
+/*console.log(str3);
+
+console.log("1" +2);
+console.log(1 +"2");
+console.log("1" + 2 + 2);
+console.log(1 + 2 + "2");    */
+
+//console.log( (3 + 4) * 5 % 3);
+
+/*console.log(+ true);
+console.log(+ "");
+
+let num1, num2, num3
+
+num1 = num2 = num3 = 2 + 2  */ // is trah nahi likha jata hai
+
+// coading ke ander readibiality sabse jaruri cheej hai
+
+let gamecounter = 100
+gamecounter++;
+console.log(gamecounter);
+++gamecounter;
+console.log(gamecounter);
+
+
+
+
+
+
+
+
+
