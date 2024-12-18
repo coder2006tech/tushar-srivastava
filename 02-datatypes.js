9a656d0f6b78be395e99c0cc1cde63d90dcc4c1d
coder2006tech <prashantsln464@gmail.com>
2024-12-18 16:32:42 +0530

    data type

---
+++ 02-datatypes.js
@@ -0,0 +1,32 @@
+"use strict"; //treat all js code as newer version
+
+//alert(3+3)// weare using nodejs , not browser
+
+console.log(3
+    +3)// code readibility should be high 
+   console.log("tushar")
+
+
+   let name = "tushar" // string data type
+   let age = 18  // number
+   let isloggedIn = false //boolean type 
+   let state;
+   
+
+   // primetive data type 
+   //number => 2 to power53
+   // bigint 
+   // string => ""
+   // boolean = true/false 
+   // null => standalone value (empty)
+   // undefined=> abhi value asign nahi hui hai
+   // symbol => unique 
+
+
+   // object
+   
+   console.log(typeof null); // object 
+   console.log(typeof undefined); // undefined  
+
+   
+
